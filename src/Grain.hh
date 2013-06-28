b14e22b088c19c8a377a37792fabe9be51b4bb8b
Hans Fugal <hans@fb.com>
2013-06-28 09:03:14 -0700

    add time to Grain.hh and a little simplification in that class

--- src/Grain.hh
+++ src/Grain.hh
@@ -4,6 +4,7 @@
 #include "Envelope.hh"
 
 #include <iostream>
+#include <jack/jack.h>
 #include <memory>
 #include <random>
 #include <vector>
@@ -12,36 +13,29 @@ template <class S = float>
 struct Grain {
   Grain(std::shared_ptr<Sndbuf<S>> buf,
         sf_count_t len,
-        std::shared_ptr<Envelope> env);
+        std::shared_ptr<Envelope> env)
+    : buf_(buf), env_(env), start_(random_start(len)), time(jack_get_time())
+  {}
 
-  S sample_at(sf_count_t pos, int channel = 0);
+  S sample_at(sf_count_t pos, int channel = 0)
+  {
+    return (S)(env_->at(pos) * buf_.samples[pos + start_ + channel]);
+  }
+
+  jack_time_t time;
 
 private:
-  sf_count_t random_start(sf_count_t len);
+  sf_count_t random_start(sf_count_t len)
+  {
+    int n = buf_->frames() - len;
+
+    if (n <= 0)
+      return 0;
+
+    return std::rand() % n;
+  }
 
   std::shared_ptr<Sndbuf<S>> buf_;
   sf_count_t start_;
   std::shared_ptr<Envelope> env_;
 };
-
-template <class S>
-Grain<S>::Grain(std::shared_ptr<Sndbuf<S>> buf, sf_count_t len, std::shared_ptr<Envelope> env)
-  : buf_(buf), env_(env), start_(random_start(len)) {}
-
-template <class S>
-sf_count_t Grain<S>::random_start(sf_count_t len)
-{
-  int n = buf_->frames() - len;
-
-  if (n <= 0)
-    return 0;
-
-  return std::rand() % n;
-}
-
-template <class S>
-S Grain<S>::sample_at(sf_count_t pos, int channel)
-{
-  return (S)(env_->at(pos) * buf_.samples[pos + start_ + channel]);
-}
-
