ca91e29822d77d0457fa36de5819aa670083bcda
Hans Fugal <fugalh@fb.com>
2013-12-13 12:47:21 -0800

    first word
    grannycap now emits OSC for the first word if it's printable

--- grannycap.py
+++ grannycap.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
+import granny
+
 import dpkt
 import pcap
-import OSC
+
 import re
 import string
+import time
 
-p = pcap.pcap()
-printable = re.compile("[%s]+" % "".join(string.printable))
+printable = re.compile("^[%s]+$" % "".join(string.printable))
+
+try:
+    p = pcap.pcap()
+except:
+    print "Got root?"
+    exit(1)
+
 p.setfilter('port 11302')
 
-for ts, pkt in p:
+gran = granny.Client(('localhost', 1337))
+
+def payload(pkt):
     eth = dpkt.ethernet.Ethernet(pkt)
-    payload = eth.data.data.data
-    words = payload.strip().split()
+    return eth.data.data.data
+
+for ts, pkt in p:
+    words = payload(pkt).rstrip().split()
     if len(words) > 0:
         word = words[0]
         if re.match(printable, word):
-            print "%s: %s" % (ts, word)
+            print word
+            gran.send(word)
