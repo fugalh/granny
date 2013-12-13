ecdcef8a6a00016e5d121764961d513fcd97229f
Hans Fugal <hans@fb.com>
2013-12-13 12:56:22 -0800

    a little commenting in theme.sh

--- data/theme.sh
+++ data/theme.sh
@@ -1,5 +1,12 @@
-[ -z "$1" ] && exit 1
-rm *.wav
-for i in $1/*.wav; do
-    ln -fs $i .
+#!/bin/bash
+# Run in your "wavs" directory, with the path to the "theme" as the first
+# argument. e.g. "mkdir wavs; cd wavs; ../data/theme.sh ../data/clangers"
+
+dir=$1
+[ -z "$dir" ] && exit 1
+
+rm -f *.wav
+
+for wav in "$dir/*.wav"; do
+    ln -fs "$wav" .
 done
